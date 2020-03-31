82cffc6fad6d3fff7f37f1607431abc08491d074
Patrick Kam <kampatrick46@gmail.com>
2020-03-31 10:21:14 -0400

    push

--- asst_2/main.js
+++ asst_2/main.js
@@ -6,8 +6,28 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for(let i = 1; i <= 5; i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic', + i + '.jpeg');
+    thumbBar.appendChild(newImage);
 
+    newImage.onclick = function(e){
+    displayedImage.src = e.target.src;  
+    }
+}
 /* Wiring up the Darken/Lighten button */
+
+btn.onclick = function(){
+    const btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark'){
+
+        btn.setAttribute('class', 'light');
+        btn.textcontent = 'Lighten';
+        overlay.getElementsByClassName.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+    else{
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.getElementsByClassName.backgroundColor = 'rgba(0,0,0,0)';
+    }
+}
