b395c5560388433af02feda4d457472823460109
Patrick Kam <kampatrick46@gmail.com>
2020-04-01 21:58:25 -0400

    push

--- asst_2/main.js
+++ asst_2/main.js
@@ -18,15 +18,15 @@ for(let i = 1; i <= 5; i++){
 /* Wiring up the Darken/Lighten button */
 
 btn.onclick = function(){
-    const btnClass = btn.getAttribute('class');
+    const btnClass = btn.getAttribute('.thumb-bar');
     if(btnClass === 'dark'){
 
-        btn.setAttribute('class', 'light');
+        btn.setAttribute('.thumb-bar', 'light');
         btn.textcontent = 'Lighten';
         overlay.getElementsByClassName.backgroundColor = 'rgba(0,0,0,0.5)';
     }
     else{
-        btn.setAttribute('class', 'dark');
+        btn.setAttribute('.thumb-bar', 'dark');
         btn.textContent = 'Darken';
         overlay.getElementsByClassName.backgroundColor = 'rgba(0,0,0,0)';
     }
